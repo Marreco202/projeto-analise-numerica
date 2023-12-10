78cb339070dfcb06abdc622c5a3bc5944233c18d
Matheus-Valejo <matheusvalejo8@gmail.com>
2023-12-09 22:45:16 -0300

    maluquice

--- src/bezier.c
+++ src/bezier.c
@@ -54,6 +54,14 @@ double get_s(double t1, double t2, double n){ // n == step
 }
 
 
-double func_ft(double s, double t, double n){
-    return s - get_s(0,t,n);
+double *func_ft(double (*fx) (double x),double (*fy) (double x),double s, double n,double a,double b){ //
+    double *vet = myvet_cria(2);
+    double meio,length;
+    while((b-a)> 10e-5){
+        meio = (a+b)/2.0;
+        length = get(0,meio,n);
+    }
+    vet[0] = fx(meio);
+    vet[1] = fy(meio);    
+    return vet;
 }
--- src/main.c
+++ src/main.c
@@ -17,8 +17,9 @@ int main(void){
     //Bezier do exemplo do slide 
     double xT[] = {0.5, 0.3, 3.9, -4.7};
     double yT[] = {1.5, 0.3, 0.9, -2.7};
-
-
+    double* vetor = ponto_x_y(func_xt,func_yt,10);
+    double length = get_s(0,10,0.1);
+    double* inverse_vetor = func_ft(func_xt,func_yt,length,0.1,0,10);
     printf("Hello there!\n");
 
     return 0;
